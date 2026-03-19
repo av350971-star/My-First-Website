7ca3d807eec23049ddfee8bbc230afedde4496ef
Ankit <av350971@gmail.com>
2026-03-19 13:59:54 +0530

    first commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>My Portfolio Website</h1>
+
+      <h2>ABOUT ME</h2>
+      <p>My Self Ankit Varma Student of Information Technology</p>
+
+      <h2>Skills</h2>
+      <ul>
+        <li>JAVA</li>
+        <li>DBMS</li>
+        <li>JDBC</li>
+      </ul>
+
+      <h2>Projects</h2>
+      <p>1. College Complaint Management System</p>
+      <p>2. Auto Tank Shoot Game (Python)</p>
+
+      <h2>Contact</h2>
+      <p>Email: av350971@gmail.com</p>
+      <p>Mob: 7987361896</p>
+    </div>
+  );
+}
+
+export default App;
